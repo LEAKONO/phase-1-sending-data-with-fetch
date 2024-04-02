9ced1fa43aaf0079e39fa08dda7799ffcab460c1
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-04-02 23:52:25 +0300

    sending fetch data

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(response => response.json())
+  .then(data => {
+    document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+    return data;
+  })
+  .catch(error => {
+    document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    console.error('Error:', error);
+    return error;
+  });
+  }
+  
